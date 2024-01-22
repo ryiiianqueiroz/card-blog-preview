4e060d75c230bd73d4d33ab7bab55619019be950
ryiiianqueiroz <ryanqueiroz2017@gmail.com>
2024-01-22 20:49:14 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'card-blog-preview' title`, () => {
+  it(`should have the 'blog-preview-card' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('card-blog-preview');
+    expect(app.title).toEqual('blog-preview-card');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, card-blog-preview');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, blog-preview-card');
   });
 });
